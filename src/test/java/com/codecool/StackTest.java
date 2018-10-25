c023894bfb1b9d9d8db958178236caa9f521d672
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-25 15:45:46 +0200

    Add test checking actual size of stack

--- src/test/java/com/codecool/StackTest.java
+++ src/test/java/com/codecool/StackTest.java
@@ -20,11 +20,21 @@ class StackTest {
     }
 
     @Test
-    void returnsValidSize() {
+    void returnsValidCapacity() {
         Stack<String> stack = new Stack<>(19);
 
         int expected = 19;
         int actual = stack.getCapacity();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void returnsValidSize_when_noObjectPushed() {
+        Stack<String> stack = new Stack<>(10);
+
+        int expected = 0;
+        int actual = stack.getSize();
 
         assertEquals(expected, actual);
     }
