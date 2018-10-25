7c248a17caf3164829faefc57bc70404534dfecd
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-25 15:06:41 +0200

    Add test checking creating stack with negative capacity

--- src/test/java/com/codecool/StackTest.java
+++ src/test/java/com/codecool/StackTest.java
@@ -1,7 +1,14 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
-
+    @Test
+    void throwsException_when_wrongCapacityPassed() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Stack<String>(-1);
+        });
+    }
 }
