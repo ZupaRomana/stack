5cdb073be6695562e69d73f87f21aec6c1455ad1
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-25 15:10:25 +0200

    Implement throwing exception when bad size passed

--- src/main/java/com/codecool/Stack.java
+++ src/main/java/com/codecool/Stack.java
@@ -1,4 +1,16 @@
 package com.codecool;
 
 public class Stack<Element> {
+
+    public Stack(int size) throws IllegalArgumentException {
+        if (wrongSize(size)) {
+            throw new IllegalArgumentException();
+        } else {
+
+        }
+    }
+
+    private boolean wrongSize(int size) {
+        return size < 0;
+    }
 }
